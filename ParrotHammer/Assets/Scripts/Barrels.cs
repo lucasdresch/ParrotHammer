71975c0c81d9a06ed400004a25311c40936ff334
Lucås Drēsch <49284431+lucasdresch@users.noreply.github.com>
2026-02-28 06:08:31 -0300

    Update features
    Add Barrel prefabs
    add scritp Barrels
    Add and simplify functions

---
+++ ParrotHammer/Assets/Scripts/Barrels.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using UnityEngine;
+
+public class Barrels : MonoBehaviour
+{
+    public void BateEsq() {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vel().x, vel().y);
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddTorque(100.0f * rot());
+        Invoke("BarrelDel", 2.0f);
+    }
+
+    public void BateDir() {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-vel().x, vel().y);
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        GetComponent<Rigidbody2D>().AddTorque(-100.0f * rot());
+        Invoke("BarrelDel", 2.0f);
+    }
+
+    public void BarrelDel() {
+        Destroy(gameObject);
+    }
+    Vector2 vel() {
+        float h = Random.Range(3, 9);
+        float d = Random.Range(3, 6);
+        return new Vector2(h, d);
+    }
+    int rot() {
+        int r = 0;
+        r = Random.Range(1, 10); Debug.Log($"r ={r}");
+        if (r > 5f) { 
+            r = 1; 
+        } else { 
+            r = -1; 
+        }Debug.Log($"r={r}");
+        return r; 
+    }
+}
--- ParrotHammer/Assets/Scripts/PlayerCtrlrs.cs
+++ ParrotHammer/Assets/Scripts/PlayerCtrlrs.cs
@@ -6,16 +6,18 @@ public class PlayerCtrlrs : MonoBehaviour
 {
     public GameObject Player, Player_idle, Player_hit;
     public GameObject InitialPos, Barril, EnemyEsq, EnemyDir;
-
-
+    [SerializeField]
+    private List<GameObject> BarrelsList;
+    private int MaxBarrels = 7;
 
     // Start is called before the first frame update
     void Start()
     {
+        BarrelsList = new List<GameObject>();
         Player = GameObject.FindGameObjectWithTag("Player");
         Player_idle = GameObject.FindGameObjectWithTag("Player_idle");
         Player_hit = GameObject.FindGameObjectWithTag("Player_hit");
-        ChecaPlayerState(true);
+        PlayerStateReturn();
         CreateBarrelStart();
     }
 
@@ -26,21 +28,25 @@ public class PlayerCtrlrs : MonoBehaviour
             if (Input.mousePosition.x < Screen.width/2) {
                 Debug.Log("Bateu esquerda!");
                 ChecaPlayerPosition(0.3f);
-                ChecaPlayerState(false);
+                PlayerStateAttack();
                 Invoke("PlayerStateReturn", 0.25f);
+                BarrelsList[0].SendMessage("BateEsq");
             }
             else {
                 Debug.Log("BSateu direita!");
                 ChecaPlayerPosition(-0.3f);
-                ChecaPlayerState(false);
+                PlayerStateAttack();
                 Invoke("PlayerStateReturn", 0.25f);
+                BarrelsList[0].SendMessage("BateDir");
             }
+            BarrelsList.RemoveAt(0);
+            RepositionBarrels();
         }
     }
 
-    public void ChecaPlayerState(bool state) {
-        Player_idle.SetActive(state); 
-        Player_hit.SetActive(!state);
+    public void PlayerStateAttack() {
+        Player_idle.SetActive(false); 
+        Player_hit.SetActive(true);
     }
     public void PlayerStateReturn() {
         Player_idle.SetActive(true);
@@ -58,7 +64,7 @@ public class PlayerCtrlrs : MonoBehaviour
     GameObject CreateNewBarrel(Vector2 pos) {
 
         GameObject newBarrel;
-        if (Random.value > 0.5f) {
+        if (Random.value > 0.5f || BarrelsList.Count < 3) {
             newBarrel = Instantiate(Barril);
         }
         else { 
@@ -75,14 +81,18 @@ public class PlayerCtrlrs : MonoBehaviour
 
     public void CreateBarrelStart() {
         
-        for (int i = 0; i < 8; i++) {
-            if (i == 0) {
-                GameObject barrel = Instantiate(Barril, InitialPos.transform);
-            }
-            else { 
-                GameObject barrel = CreateNewBarrel(new Vector2(0, -2.13f + (i * 0.99f)));
-            }
+        for (int i = 0; i < MaxBarrels; i++) {
+            GameObject barrel = CreateNewBarrel(new Vector2(0, -2.13f + (i * 0.99f)));
+            BarrelsList.Add(barrel);
         }
         
     }
+    
+    public void RepositionBarrels() {
+        GameObject newBarrel = CreateNewBarrel(new Vector2(0, -2.13f + ((MaxBarrels) * 0.99f)));
+        BarrelsList.Add(newBarrel);
+        for (int i = 0; i < MaxBarrels; i++) {
+            BarrelsList[i].transform.position = new Vector2(BarrelsList[i].transform.position.x, BarrelsList[i].transform.position.y - 0.99f);
+        }
+    }// */
 }
