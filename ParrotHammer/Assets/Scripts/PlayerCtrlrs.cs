efe0a60aa2b8785ccfb3e780a7df833082479005
Lucås Drēsch <49284431+lucasdresch@users.noreply.github.com>
2026-03-10 09:51:59 -0300

    update functions
    Add
    CheckHit
    CheckKill
    CheckDeath
    GetScore
    GetTopScore

--- ParrotHammer/Assets/Scripts/PlayerCtrlrs.cs
+++ ParrotHammer/Assets/Scripts/PlayerCtrlrs.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCtrlrs : MonoBehaviour
 {
@@ -10,19 +12,36 @@ public class PlayerCtrlrs : MonoBehaviour
     private List<GameObject> BarrelsList;
     private int MaxBarrels = 7;
     [SerializeField]
-    private bool lado;
+    private bool lado, start, end;
     // Start is called before the first frame update
+    [SerializeField]
+    private Text info, top;
+    [SerializeField]
+    private int score, topScore, kill;
     void Start() {
         BarrelsList = new List<GameObject>();
         Player = GameObject.FindGameObjectWithTag("Player");
         Player_idle = GameObject.FindGameObjectWithTag("Player_idle");
         Player_hit = GameObject.FindGameObjectWithTag("Player_hit");
+        info = GameObject.FindGameObjectWithTag("GameInfo").GetComponent<Text>();
+        top = GameObject.FindGameObjectWithTag("TopInfo").GetComponent<Text>();
+        info.transform.position = new Vector2(Screen.width / 2, (Screen.height / 2));
+        top.transform.position = new Vector2(Screen.width / 2 + 150, (Screen.height / 2) + 100);
+        info.GetComponent<Text>().text = "Toque para começar!";
+        top.GetComponent<Text>().text = "Top Score: ";
         PlayerStateReturn();
         CreateBarrelStart();
+        start = false;
+        score = 0;
+        kill = 0;
     }
 
     // Update is called once per frame
     void Update() {
+        if (info.GetComponent<Text>().text != "Toque para começar!" && start == false) {
+            start = true;
+            info.rectTransform.position = new Vector2(Screen.width / 2, (Screen.height / 2) + 100);
+        }
         if (Input.GetButtonDown("Fire1")) {
             if (Input.mousePosition.x < Screen.width / 2) {
                 lado = false;
@@ -40,9 +59,9 @@ public class PlayerCtrlrs : MonoBehaviour
                 Invoke("PlayerStateReturn", 0.25f);
                 BarrelsList[0].SendMessage("BateDir");
             }
+            CheckHit();
             BarrelsList.RemoveAt(0);
             RepositionBarrels();
-            CheckHit();
         }
     }
 
@@ -82,7 +101,6 @@ public class PlayerCtrlrs : MonoBehaviour
     }
 
     public void CreateBarrelStart() {
-
         for (int i = 0; i < MaxBarrels; i++) {
             GameObject barrel = CreateNewBarrel(new Vector2(0, -2.13f + (i * 0.99f)));
             BarrelsList.Add(barrel);
@@ -96,15 +114,38 @@ public class PlayerCtrlrs : MonoBehaviour
         for (int i = 0; i < MaxBarrels; i++) {
             BarrelsList[i].transform.position = new Vector2(BarrelsList[i].transform.position.x, BarrelsList[i].transform.position.y - 0.99f);
         }
+        CheckDeath();
+    }
+    public void CheckHit() {
+        if (BarrelsList[0].gameObject.layer == 10) {
+            GetScore();
+            CheckKill();
+        }
     }
 
-    public void CheckHit() { 
-        if (BarrelsList[0].gameObject.CompareTag("EnemyEsq") && lado) {
+    public void CheckKill() {
+        if (BarrelsList[0].gameObject.CompareTag("EnemyEsq") || BarrelsList[0].gameObject.CompareTag("EnemyDir")) {
             Debug.Log("Matou enemy!");
-        } 
-        else if(BarrelsList[0].gameObject.CompareTag("EnemyDir") && !lado) {
-            Debug.Log("Morreu pro enemy!");
+            kill++;
         }
+        if(kill > 9) {
+            kill = 0;
+            // restaura time
+        }
+    }
+    public void CheckDeath() {
+        if ((BarrelsList[0].gameObject.CompareTag("EnemyEsq") && !lado) || (BarrelsList[0].gameObject.CompareTag("EnemyDir") && lado)) {
+            Debug.Log("Morreu!");
+            GetTopScore();
+        }
+    }
+    public void GetScore() {
+        score++;
+        info.GetComponent<Text>().text = score.ToString();
+    }
+    public void GetTopScore() {
+        topScore = score;
+        top.GetComponent<Text>().text = $"Top Score: {topScore}";
     }
 
 }
